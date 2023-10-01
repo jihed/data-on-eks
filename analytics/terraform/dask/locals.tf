f2ad5f107cfdc0ce354d48a7cb14517691f7b9cb
JIHED MSELMI <jihed.mselmi@gmail.com>
2023-10-01 18:09:24 +0200

    update blueprints

---
+++ analytics/terraform/dask/locals.tf
@@ -0,0 +1,24 @@
+
+data "aws_ecrpublic_authorization_token" "token" {
+  provider = aws.ecr
+}
+
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+data "aws_partition" "current" {}
+
+locals {
+  name     = var.name
+  region   = var.region
+  vpc_cidr = var.vpc_cidr
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  account_id = data.aws_caller_identity.current.account_id
+  partition  = data.aws_partition.current.partition
+
+  tags = {
+    Blueprint  = local.name
+    GithubRepo = "github.com/awslabs/data-on-eks"
+  }
+}
